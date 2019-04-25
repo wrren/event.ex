326120882b5c473d81d67453bab1ac2df97c8fa3
Warren Kenny <warren.kenny@gmail.com>
2019-04-25 22:36:41 +0100

    Added initial implementation of event adapter

---
+++ lib/event/adapter.ex
@@ -0,0 +1,33 @@
+defmodule Event.Adapter do
+  @moduledoc """
+  Acts as an event sink, sending all incoming events as messages to the provided
+  process. Useful when you have a process that should receive events but cannot act
+  as an event sink or GenStage.
+
+  The adapter process will monitor the receiver process and exit when it does.
+  """
+  use Event.Sink
+
+  @doc """
+  Start an Event Adapter that will receive events from the given source GenStage and
+  send them as messages to the receiver pid provided
+  """
+  def start_link(source, receiver) do
+    Event.Sink.start_link(__MODULE__, receiver, [subscribe_to: source])
+  end
+
+  def init(receiver) do
+    Process.monitor(receiver)
+    {:ok, receiver}
+  end
+
+  def handle_events(events, _from, receiver) do
+    events
+    |> Enum.each(&(send(receiver, &1)))
+    {:noreply, receiver}
+  end
+
+  def handle_info({:DOWN, _ref, :process, receiver, _}, receiver) do
+    {:stop, :normal, receiver}
+  end
+end
